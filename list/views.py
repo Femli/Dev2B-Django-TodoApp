6eaac064bb67a34f2e4166ef1de8013214557bbe
Femli <22228100+Femli@users.noreply.github.com>
2020-10-22 23:42:19 -0700

    adding the form -- but still need to finish the POST request

--- list/views.py
+++ list/views.py
@@ -4,6 +4,16 @@ from datetime import datetime
 
 ## The functions below will provide specific view to our application
 
+
+## Example 1 - hello word with the current time
+def index(request): ## keep the name of this view function in mind, as it will be imported in another file 
+    now = datetime.now()
+    current_time = now.strftime("%H:%M:%S")
+    return HttpResponse("Hello World! \n The current time is: %s" % (current_time))
+
+def potatoes(request):
+    return HttpResponse("Potatoes are good!")
+
 def hotdog(request):
     return HttpResponse("Welcome to my hotdog shack! Please select one of the following dogs: 1) Regular 2) Mexican Style 3) Veggie")
 
@@ -19,28 +29,30 @@ def choice(request, menu_id):
         price = "$3.50"
     return HttpResponse("You have selected a {0} dog! The total will be {1}".format(option, price))
 
-def services(request):
-    first = "Franco"
-    last = "Sanchez"
-    top_three = ["Pokemon", "God of War", "League of Legends"]
-    context = {'first_name':first, 'last_name':last, 'top_three_games':top_three} #We need to store the variables we want to pass into a dictionary, it's the only way DTL can render it
-    return render(request, 'list/services.html', context)
+def hello(request):
+    return render(request, 'list/index.html')
 
-## rendering our tasks view
+def home(request):
+    return render(request, 'list/homepage.html')
+
+def contactus(request):
+    return render(request, 'list/contactus.html')
+
+# this is taking in the information about our tasks, given by the model file
 from .models import *
 from .forms import *
-from django.shortcuts import redirect
 
-def taskview(request):
+def services(request):
+    first = "Franky"
+    last = "Tanky"
+    top_four = ["Pokemon", "God of War", "League of Legends", "Spyro"]
+    context = {'first_name':first, 'last_name':last, 'top_four_games':top_four} #We need to store the variables we want to pass into a dictionary, it's the only way DTL can render it
+
     tasks = Task.objects.all()
-
     form = TaskForm()
-
-    if request.method == "POST":
-        form = TaskForm(request.POST)
-        if form.is_valid():
-            form.save()
-        return redirect('/')
-
     context = {'tasks': tasks, 'form': form}
+
     return render(request, 'list/services.html', context)
+
+def child(request):
+    return render(request, 'list/child.html')
