07b48618c8fa3980154f7343dcd45a1adf425831
Femli <22228100+Femli@users.noreply.github.com>
2020-10-27 21:53:27 -0700

    rendered the tasks on our homepage and can add tasks via post method

--- list/urls.py
+++ list/urls.py
@@ -2,11 +2,5 @@ from django.urls import path
 from . import views ## in this same folder, we are importing the objects in views.py
 
 urlpatterns = [
-    path('', views.hotdog, name='hotdog'), ## views.index is the name of the function housing our http request
-    path('<int:menu_id>/', views.choice, name='choice'),
-    path('hello/', views.hello, name="hello"),
-    path('home/', views.home, name="home"),
-    path('contact-us/', views.contactus, name="contact-us"),
-    path('services/', views.services, name='services'),
-    path('child/', views.child, name="child")
+    path('', views.index, name='tasks'),
 ]
--- list/views.py
+++ list/views.py
@@ -1,58 +1,25 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from datetime import datetime
 
 ## The functions below will provide specific view to our application
 
 
-## Example 1 - hello word with the current time
-def index(request): ## keep the name of this view function in mind, as it will be imported in another file 
-    now = datetime.now()
-    current_time = now.strftime("%H:%M:%S")
-    return HttpResponse("Hello World! \n The current time is: %s" % (current_time))
-
-def potatoes(request):
-    return HttpResponse("Potatoes are good!")
-
-def hotdog(request):
-    return HttpResponse("Welcome to my hotdog shack! Please select one of the following dogs: 1) Regular 2) Mexican Style 3) Veggie")
-
-def choice(request, menu_id):
-    if menu_id == 1:
-        option = "Regular"
-        price = "$1.50"
-    elif menu_id == 2:
-        option = "Mexican Style"
-        price = "$1.50"
-    elif menu_id == 3:
-        option = "Veggie"
-        price = "$3.50"
-    return HttpResponse("You have selected a {0} dog! The total will be {1}".format(option, price))
-
-def hello(request):
-    return render(request, 'list/index.html')
-
-def home(request):
-    return render(request, 'list/homepage.html')
-
-def contactus(request):
-    return render(request, 'list/contactus.html')
-
 # this is taking in the information about our tasks, given by the model file
 from .models import *
 from .forms import *
 
-def services(request):
-    first = "Franky"
-    last = "Tanky"
-    top_four = ["Pokemon", "God of War", "League of Legends", "Spyro"]
-    context = {'first_name':first, 'last_name':last, 'top_four_games':top_four} #We need to store the variables we want to pass into a dictionary, it's the only way DTL can render it
+def index(request):
 
     tasks = Task.objects.all()
     form = TaskForm()
+
+    if request.method == 'POST':
+        form = TaskForm(request.POST)
+        if form.is_valid():
+            form.save()
+        return redirect('/')
+
     context = {'tasks': tasks, 'form': form}
 
     return render(request, 'list/services.html', context)
-
-def child(request):
-    return render(request, 'list/child.html')
