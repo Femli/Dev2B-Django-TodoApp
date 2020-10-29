6ef4b76ed52fe0f5c7f6a4f96eab8d822e257193
Femli <22228100+Femli@users.noreply.github.com>
2020-10-29 16:16:23 -0700

    updating and deleting tasks with POST method

--- list/urls.py
+++ list/urls.py
@@ -3,4 +3,6 @@ from . import views ## in this same folder, we are importing the objects in view
 
 urlpatterns = [
     path('', views.index, name='tasks'),
+    path('update/<str:pk>', views.updateTask, name="update_tasks"),
+    path('delete/<str:pk>', views.deleteTask, name="delete_tasks")
 ]
--- list/views.py
+++ list/views.py
@@ -10,7 +10,6 @@ from .models import *
 from .forms import *
 
 def index(request):
-
     tasks = Task.objects.all()
     form = TaskForm()
 
@@ -23,3 +22,27 @@ def index(request):
     context = {'tasks': tasks, 'form': form}
 
     return render(request, 'list/services.html', context)
+
+def updateTask(request, pk): #'pk' is going to be the 'primary key', which is the row id in the database, of our tasks.
+    task = Task.objects.get(id=pk) # We will use this to order our tasks, and make the url route dynamic to each task
+
+    form = TaskForm(instance=task) #the 'instance=task' allows us to pre-fill a form with our task attributes
+
+    if request.method == 'POST':
+        form = TaskForm(request.POST, instance=task) # we don't want to create a new form, but rather use the already populated form with our task attributes
+        if form.is_valid():
+            form.save()
+        return redirect('/')
+
+    context = {'form':form}
+    return render(request, 'list/update.html', context)
+
+def deleteTask(request, pk):
+    item = Task.objects.get(id=pk) # this will allow us to pass the title of our task, so we can confirm it's deletion
+
+    if request.method == "POST":
+        item.delete() # this will remove the task from our database
+        return redirect('/')
+
+    context = {'item':item}
+    return render(request, 'list/delete.html', context)
