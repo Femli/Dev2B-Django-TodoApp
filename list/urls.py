67daa4c782e93557ea500369b6b15e04f7ebe5a4
Femli <22228100+Femli@users.noreply.github.com>
2020-11-05 18:01:20 -0800

    css code for the app

--- list/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views ## in this same folder, we are importing the objects in views.py
-
-urlpatterns = [
-    path('', views.index, name='tasks'),
-    path('update/<str:pk>', views.updateTask, name='update_tasks'),
-    path('delete/<str:pk>', views.deleteTask, name='delete_tasks')
-]
